c97a8b5acc1e6673c33ff51dea508950cfe8d8c7
Ronny <ronny.icne@gmail.com>
2021-05-16 10:33:29 -0700

    Configuration files. Utilizing esbuild.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import {useState, useEffect, useRef} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {unpkgPathPlugin} from './plugins/unpkg-path-plugin';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const startService = async () => {
+        //we can reference ref.current anywhere inside the component
+        ref.current = await esbuild.startService({
+            //this allows you to use esbuild service
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+
+        })
+    }
+    useEffect(() => {
+        startService();
+    //[] means we are going to run this once
+    }, [])
+
+    const onClick = async () => {
+        //if there is no ref.current then it returns early
+        if(!ref.current){
+            return;
+        }
+
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        });
+        setCode(result.code)
+    }
+    return <div>
+        <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
